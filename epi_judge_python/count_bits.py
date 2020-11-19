a8a9731159cc99add6d015e988d07a294bf584ea
pbutti <pierfrancesco.butti@gmail.com>
2020-11-18 18:46:18 -0800

    Count bits solved

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -3,7 +3,12 @@ from test_framework import generic_test
 
 def count_bits(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    count = 0
+    while x > 0:
+        count += x & 1
+        x >>= 1
+
+    return count
 
 
 if __name__ == '__main__':
